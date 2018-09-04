f6081dd3a26af09eef8527cf2c9ce6a22f04a59f
William A. Kennington III <william@wkennington.com>
2018-09-04 15:10:36 -0700

    Make symbol tables available

--- base.cpp
+++ base.cpp
@@ -153,6 +153,52 @@ class Properties
 {
 };
 
+template <>
+class Properties<EncodingType::Base16>
+{
+	protected:
+		static constexpr auto symbols = []() {
+			std::array<char, 16> ret{};
+			for (size_t i = 0; i < 10; ++i)
+				ret[i] = '0' + i;
+			for (size_t i = 0; i < 6; ++i)
+				ret[i + 10] = 'A' + i;
+			return ret;
+		}();
+};
+
+template <>
+class Properties<EncodingType::Base32>
+{
+	protected:
+		static constexpr auto symbols = []() {
+			std::array<char, 32> ret{};
+			for (size_t i = 0; i < 26; ++i)
+				ret[i] = 'A' + i;
+			for (size_t i = 0; i < 6; ++i)
+				ret[i + 26] = '0' + i;
+			return ret;
+		}();
+};
+
+template <>
+class Properties<EncodingType::Base64>
+{
+	protected:
+		static constexpr auto symbols = []() {
+			std::array<char, 64> ret{};
+			for (size_t i = 0; i < 26; ++i)
+				ret[i] = 'A' + i;
+			for (size_t i = 0; i < 26; ++i)
+				ret[i + 26] = 'a' + i;
+			for (size_t i = 0; i < 10; ++i)
+				ret[i + 52] = '0' + i;
+			ret[62] = '+';
+			ret[63] = '/';
+			return ret;
+		}();
+};
+
 template <EncodingType type>
 class Common : public Properties<type>
 {
